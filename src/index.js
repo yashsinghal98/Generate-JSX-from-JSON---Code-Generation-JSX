aea86d538f4bbbc4ea05e90bd2108895fa1fbcd5
yashsinghal98 <68867021+yashsinghal98@users.noreply.github.com>
2020-10-26 19:14:19 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+function recur(obj){
+    return(
+        <>
+        <obj.type name={obj.name} root={obj.root} style={obj.style}>
+            {obj.children!==undefined?recur(obj.children):null}
+        </obj.type>
+        </>
+    );
+}
 function generateCodeFromObject(obj){
     //return a code generated string
+    return (
+        <>
+        {recur(obj)}
+        </>
+    )
    }
    
 module.exports=generateCodeFromObject;
 
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
